93df9ad2654ee4f67d0304829fce521dbb2b377c
Yaser Martinez Palenzuela <yaser.martinez@gmail.com>
2013-04-27 18:58:30 +0200

    deleted bit_add function

--- ex1/test_ex1.py
+++ ex1/test_ex1.py
@@ -5,11 +5,6 @@ def test_submit():
     assert len(submit.validParts) == len(submit.sources)
 
 
-def test_bitadd():
-    assert submit.bitadd(64, 2*2*2*2*2) == 96
-    assert submit.bitadd(100000000000, -123) == 4294967295
-
-
 def test_sha1():
     assert submit.sha1('abcd') == '81fe8bfe87576c3ecb22426f8e57847382917acf'  # Value taken from from Matlab version
 
